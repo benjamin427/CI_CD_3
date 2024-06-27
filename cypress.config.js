49111c0e5b306c6cb175d4130c99f3691a76fd3a
Benjamin_79 <shadowboxer79@hotmail.com>
2024-06-27 14:19:30 -0400

    Configuration file

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+const createBuilder = require('@bahmutov/cypress-esbuild-preprocessor')
+const addCucumberpreprocessor = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      const build = createBuilder({
+        plugins: [createEsbuildPlugin(config)]
+      })
+      on('file:preprocessor', build)
+      addCucumberpreprocessor(on, config)
+      return config;
+    },
+    specPattern: "**/*.feature",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      quite: false,
+      json: true
+    }
+  },
+});
